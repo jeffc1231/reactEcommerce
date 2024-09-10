754b120f1ca2fcf19f657b80b3626daea3d4ffd0
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-09-10 15:37:36 -0400

    initial context api setup

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import Product from './Product';
 import Title from './Title';
+import { storeProducts } from '../data';
+import { ProductConsumer } from '../context';
 export default class ProductList extends Component {
   state={
-    products:[]
+    products: storeProducts
   }
   render() {
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
+            <Title name="our" title="products"/>
+
             <div className="row">
-              <Title name="our" title="products"/>
+              <ProductConsumer>
+                {(hello) => {
+                  return <h1>{hello}</h1>
+                }}
+              </ProductConsumer>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
+import { ProductProvider } from './context';
+import { ProductConsumer } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
