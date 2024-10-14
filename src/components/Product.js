604276b2fea872e4e479a36cd84542474368d623
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-10-14 16:21:50 -0400

    initial modal added

--- src/components/Product.js
+++ src/components/Product.js
@@ -22,7 +22,11 @@ export default class Product extends Component {
               </Link>
               <button className='cart-btn' 
               disabled={inCart ? true : false}
-              onClick={() => value.addToCart(id)}
+              onClick={() => {
+                value.addToCart(id);
+                value.openModal(id);
+              }
+            }
               >
               {inCart?
                 (<p className='text-capitalize mb-0' disabled> {" "}in Cart</p>)
