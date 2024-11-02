1eb84b2814f26e4a741f1d4a5edcb8c2f730c510
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-11-01 21:52:07 -0400

    cart folder added

--- src/context.js
+++ src/context.js
@@ -12,6 +12,9 @@ state ={
     cart: [],
     modalOpen:false,
     modalProduct:detailProduct,
+    cartSubTotal:0,
+    cartTax:0,
+    cartTotal:0,
 };
 componentDidMount() {
     this.setProducts();
@@ -67,6 +70,22 @@ closeModal = () => {
         return{modalOpen:false}
     })
 }
+increment = (id) => {
+    console.log('this is increment method');
+    
+}
+decrement = (id) => {
+    console.log('this is decrement method');
+    
+}
+removeItem = (id) => {
+    console.log('item removed');
+    
+}
+clearCart = () => {
+    console.log('cart was cleared');
+    
+}
   render() {
     return (
       <ProductContext.Provider value={{
@@ -74,7 +93,11 @@ closeModal = () => {
         handleDetail:this.handleDetail,
         addToCart: this.addToCart,
         openModal: this.openModal,
-        closeModal: this.closeModal
+        closeModal: this.closeModal,
+        increment: this.increment,
+        decrement: this.decrement,
+        removeItem: this.removeItem,
+        clearCart: this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
