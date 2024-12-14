f1c72e7dab0a26f796a6dcca1bfc5b0af993c929
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-12-13 20:04:37 -0500

    increment and decrement components added

--- src/context.js
+++ src/context.js
@@ -71,16 +71,54 @@ closeModal = () => {
     })
 }
 increment = (id) => {
-    console.log('this is increment method');
-    
+    let tempCart = [...this.state.cart];
+const selectedProduct = tempCart.find(item=>item.id === id)
+const index = tempCart.indexOf(selectedProduct);
+const product = tempCart[index];
+product.count = product.count + 1;
+product.total = product.price * product.count;   
+
+this.setState(() => {return{cart:[...tempCart]}}, () => {this.addTotals()})
+
 }
 decrement = (id) => {
-    console.log('this is decrement method');
-    
+    let tempCart = [...this.state.cart];
+const selectedProduct = tempCart.find(item=>item.id === id)
+const index = tempCart.indexOf(selectedProduct);
+const product = tempCart[index];
+
+product.count = product.count - 1;
+
+if(product.count === 0){
+    this.removeItem(id);
+} else {
+    product.total = product.count * product.price;
+    this.setState(() => {return{cart:[...tempCart]}}, () => {this.addTotals()})
+}
+
+
 }
 removeItem = (id) => {
-    console.log('item removed');
-    
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+        return {
+            cart:[...tempCart],
+            products:[...tempProducts]
+        }
+    },() => {
+        this.addTotals();
+    }
+    );
 }
 clearCart = () => {
     console.log('cart was cleared');
