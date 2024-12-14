561014e9cc40fbbef26b940f63b29acb9447b81d
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-12-13 20:36:04 -0500

    404 error page added

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h3>page not found</h3>
+      <div className='container'>
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">
+              404
+            </h1>
+            <h2>error</h2>
+            <h1>page not found</h1>
+            {/* <h3>
+              the requested url 
+              <span className='text-danger'>
+                {this.props.location.pathname}
+              </span>{" "} 
+              was not found
+            </h3> */}
+          </div>
+        </div>
       </div>
     )
   }
